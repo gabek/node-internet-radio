a248ae2e35d627eba8b7c6235d74c995c18641e5
Gabe Kangas <gabek@real-ity.com>
2015-07-05 12:38:33 -0700

    Remove track name fixer

--- index.js
+++ index.js
@@ -78,24 +78,6 @@ function getStationInfo(url, callback, method) {
       return callback(null, null);
     }
   }
-
-}
-// To fix any "Beatles, The" scenarios
-function fixTrackTitle(trackString) {
-  if (trackString.split(",").length > 1) {
-    var titleArtist = trackString.split(",")[0];
-    var titleSong = trackString.split(",")[1];
-
-    // Fix the "The" issue
-    if (trackString.indexOf(", The -") !== -1) {
-      titleSong = trackString.split(",")[1].split(" - ")[1];
-      titleArtist = "The " + titleArtist;
-    }
-
-    return titleArtist + " - " + titleSong;
-  } else {
-    return trackString;
-  }
 }
 
 module.exports.StreamSource = StreamSource;
