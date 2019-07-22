891eae172d9794eaa013c458314688c1914b3e96
dbbring <dev.dbbring@gmail.com>
2019-07-22 09:20:18 -0500

    typo in module exports / async func in index

--- index.js
+++ index.js
@@ -1,87 +1,138 @@
-var async = require('async');
-var icecast = require('./lib/icecast.js');
-var shoutcast = require('./lib/shoutcast.js');
-var icystream = require('./lib/icystream.js');
-var radionomy = require('./lib/radionomy');
+var async = require("async");
+var icecast = require("./lib/icecast.js");
+var shoutcast = require("./lib/shoutcast.js");
+var icystream = require("./lib/icystream.js");
+var radionomy = require("./lib/radionomy");
 
 var StreamSource = {
-    SHOUTCAST_V1: "SHOUTCAST_V1",
-    SHOUTCAST_V2: "SHOUTCAST_V2",
-    STREAM: "STREAM",
-    ICECAST: "ICECAST",
-    RADIONOMY: "RADIONOMY"
+  SHOUTCAST_V1: "SHOUTCAST_V1",
+  SHOUTCAST_V2: "SHOUTCAST_V2",
+  STREAM: "STREAM",
+  ICECAST: "ICECAST",
+  RADIONOMY: "RADIONOMY"
 };
 
 function getStationInfo(url, callback, method) {
-    var methodHandler = undefined;
+  var methodHandler = undefined;
 
-    switch (method) {
-        case StreamSource.SHOUTCAST_V1:
-            methodHandler = shoutcast.getShoutcastV1Station;
-            break;
-        case StreamSource.SHOUTCAST_V2:
-            methodHandler = shoutcast.getShoutcastV2Station;
-            break;
-        case StreamSource.ICECAST:
-            methodHandler = icecast.getIcecastStation;
-            break;
-        case StreamSource.STREAM:
-            methodHandler = icystream.getStreamStation;
-            break;
-        case StreamSource.RADIONOMY:
-            methodHandler = radionomy.getRadionomyStation;
-        default:
-        // No specific handler. Try them all below.
-    }
+  switch (method) {
+    case StreamSource.SHOUTCAST_V1:
+      methodHandler = shoutcast.getShoutcastV1Station;
+      break;
+    case StreamSource.SHOUTCAST_V2:
+      methodHandler = shoutcast.getShoutcastV2Station;
+      break;
+    case StreamSource.ICECAST:
+      methodHandler = icecast.getIcecastStation;
+      break;
+    case StreamSource.STREAM:
+      methodHandler = icystream.getStreamStation;
+      break;
+    case StreamSource.RADIONOMY:
+      methodHandler = radionomy.getRadionomyStation;
+    default:
+    // No specific handler. Try them all below.
+  }
 
-    // If we have a specific method to fetch from then
-    // attempt only that.
-    if (methodHandler) {
-        return methodHandler(url, callback);
-    }
+  // If we have a specific method to fetch from then
+  // attempt only that.
+  if (methodHandler) {
+    return methodHandler(url, callback);
+  }
 
-    // Otherwise try them all
-    async.parallel([
-        function (asyncCallback) {
-            shoutcast.getShoutcastV1Station(url, function (error, station) {
-                asyncResultReturned(error, station, asyncCallback);
-            });
-        },
-        function (asyncCallback) {
-            shoutcast.getShoutcastV2Station(url, function (error, station) {
-                asyncResultReturned(error, station, asyncCallback);
-            });
-        },
-        function (asyncCallback) {
-            icecast.getIcecastStation(url, function (error, station) {
-                asyncResultReturned(error, station, asyncCallback);
-            });
-        },
-        function (asyncCallback) {
-            icystream.getStreamStation(url, function (error, station) {
-                asyncResultReturned(error, station, asyncCallback);
-            });
-        }
-    ], function (error, results) {
-        var stations = [];
-        if (Array.isArray(results)) {
-            stations = results.filter(Boolean);
-        }
-        if (stations.length > 0) {
-            var station = stations[0];
-            return callback(null, station);
-        } else {
-            return callback(new Error("Not able to fetch station data via any available methods."));
-        }
+  findStation(url, callback)
+    .then(x => {
+      console.log(x);
+    })
+    .catch(err => {
+      console.log(err);
     });
 
-    function asyncResultReturned(error, station, callback) {
-        if (station) {
-            return callback(true, station);
-        } else {
-            return callback(null, null);
-        }
+  async function findStation(url, callback) {
+    this.results = await V1(url);
+
+    if (this.results == null || this.results == "undefined") {
+      this.results = await V2(url);
     }
+    if (this.results == null || this.results == "undefined") {
+      this.results = await Ice(url);
+    }
+    if (this.results == null || this.results == "undefined") {
+      this.results = await Icy(url);
+    }
+
+    // Async functions for use with Promise Wrapper functions
+    function V1(url) {
+      shoutcast._getShoutcastV1Station(url, function(error, station) {
+        return station;
+      });
+    }
+    function V2(url) {
+      shoutcast._getShoutcastV2Station(url, function(error, station) {
+        return station;
+      });
+    }
+    function Icy(url) {
+      icystream.getStreamStation(url, function(error, station) {
+        return station;
+      });
+    }
+    function Ice(url) {
+      icecast._getIcecastStation(url, function(error, station) {
+        return station;
+      });
+    }
+
+    return this.results;
+  }
+
+  /* Otherwise try them all
+  async.parallel(
+    [
+      function(asyncCallback) {
+        shoutcast.getShoutcastV1Station(url, function(error, station) {
+          asyncResultReturned(error, station, asyncCallback);
+        });
+      },
+      function(asyncCallback) {
+        shoutcast.getShoutcastV2Station(url, function(error, station) {
+          asyncResultReturned(error, station, asyncCallback);
+        });
+      },
+      function(asyncCallback) {
+        icecast.getIcecastStation(url, function(error, station) {
+          asyncResultReturned(error, station, asyncCallback);
+        });
+      },
+      function(asyncCallback) {
+        icystream.getStreamStation(url, function(error, station) {
+          asyncResultReturned(error, station, asyncCallback);
+        });
+      }
+    ],
+    function(error, results) {
+      var stations = [];
+      if (Array.isArray(results)) {
+        stations = results.filter(Boolean);
+      }
+      if (stations.length > 0) {
+        var station = stations[0];
+        return callback(null, station);
+      } else {
+        return callback(
+          new Error("Not able to fetch station data via any available methods.")
+        );
+      }
+    }
+  );
+
+  function asyncResultReturned(error, station, callback) {
+    if (station) {
+      return callback(true, station);
+    } else {
+      return callback(null, null);
+    }
+  }*/
 }
 
 module.exports.StreamSource = StreamSource;
