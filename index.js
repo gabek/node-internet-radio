147486996218b694c2e0ba0eb56943d4da433b05
majimus <majimusprime@gmail.com>
2021-06-29 00:29:47 -0500

    address memory leaks

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var StreamSource = {
   SHOUTCAST_V1: 'SHOUTCAST_V1',
   SHOUTCAST_V2: 'SHOUTCAST_V2',
   STREAM: 'STREAM',
-  ICECAST: 'ICECAST'
+  ICECAST: 'ICECAST',
 };
 
 function getStationInfo(url, callback, method) {
@@ -38,10 +38,10 @@ function getStationInfo(url, callback, method) {
   // We shouldnt mix callbacks and promises but for backwards compatability I am breaking
   // the law here......
   return findStation(url)
-    .then(station => {
+    .then((station) => {
       return callback(null, station);
     })
-    .catch(err => {
+    .catch((err) => {
       return callback(err);
     });
 
@@ -50,7 +50,8 @@ function getStationInfo(url, callback, method) {
   @returns -> mixed (object if successful, string if error)
   */
   async function findStation(url) {
-    this.results = await V1(url);
+    // Start the search for results
+    this.results = await Icy(url);
     // Find which provider has our station
     if (this.results == null || typeof this.results == 'undefined') {
       this.results = await V2(url);
@@ -59,7 +60,7 @@ function getStationInfo(url, callback, method) {
       this.results = await Ice(url);
     }
     if (this.results == null || typeof this.results == 'undefined') {
-      this.results = await Icy(url);
+      this.results = await V1(url);
     }
     return this.results;
 
@@ -69,7 +70,7 @@ function getStationInfo(url, callback, method) {
     function V1(url) {
       return new Promise((resolve, reject) => {
         try {
-          shoutcast.getShoutcastV1Station(url, function(error, station) {
+          shoutcast.getShoutcastV1Station(url, function (error, station) {
             resolve(station);
           });
         } catch (err) {
@@ -80,7 +81,7 @@ function getStationInfo(url, callback, method) {
     function V2(url) {
       return new Promise((resolve, reject) => {
         try {
-          shoutcast.getShoutcastV2Station(url, function(error, station) {
+          shoutcast.getShoutcastV2Station(url, function (error, station) {
             resolve(station);
           });
         } catch (err) {
@@ -91,7 +92,7 @@ function getStationInfo(url, callback, method) {
     function Icy(url) {
       return new Promise((resolve, reject) => {
         try {
-          icystream.getStreamStation(url, function(error, station) {
+          icystream.getStreamStation(url, function (error, station) {
             resolve(station);
           });
         } catch (err) {
@@ -102,7 +103,7 @@ function getStationInfo(url, callback, method) {
     function Ice(url) {
       return new Promise((resolve, reject) => {
         try {
-          icecast.getIcecastStation(url, function(error, station) {
+          icecast.getIcecastStation(url, function (error, station) {
             resolve(station);
           });
         } catch (err) {
